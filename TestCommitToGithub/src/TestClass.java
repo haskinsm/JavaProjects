ce084b69baf13c71ca0bd2f1c858a4c89141d0cc
haskinsm <60662825+haskinsm@users.noreply.github.com>
2020-10-04 16:59:36 +0100

    Test 4 (5)

--- TestCommitToGithub/src/TestClass.java
+++ TestCommitToGithub/src/TestClass.java
@@ -3,5 +3,5 @@ public class TestClass {
   //This is a Test Initial Commit
 	//Test desktop Github #2
 	//Test pull from Github to Eclipse #3
-	//Test push Conflict, i.e. push changed file to file already differently changed on github #4
+	//Test push Conflict, i.e. push changed file to file already differently changed on github #4 #5
 }
