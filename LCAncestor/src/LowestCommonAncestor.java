4cfdbf5e236e311f08f5e84b3cea34c6583066fc
haskinsm <60662825+haskinsm@users.noreply.github.com>
2020-10-26 21:49:42 +0000

    Basic set up finished. Retaining old version suitable for Binary Trees and creating new one suitable for DAG Graphs

--- LCAncestor/src/LowestCommonAncestor.java
+++ LCAncestor/src/LowestCommonAncestor.java
@@ -1,59 +1,60 @@
-//Test Branching 22222
+import java.util.ArrayList;
+
+//Test Branching
 //LCA = Lowest Common Ancestor. Presume not allowed to use BST, but would be quicker if was allowed to use that data structure.
-class TreeNode{
+class DAGTreeNode{
 	int data;
-	TreeNode left, right;
-	
-	TreeNode(int data){
+	ArrayList<DAGTreeNode> successors;
+
+	DAGTreeNode(int data){
 		this.data = data;
-		this.left = this.right = null;
+		successors = new ArrayList<DAGTreeNode>();
+	}
+}
+
+class BinaryTreeNode{
+	int data;
+	BinaryTreeNode right, left;
+	
+	BinaryTreeNode(int data){
+		this.data = data;
+		this.right = this.left = null;
 	}
 }
 
 public class LowestCommonAncestor {
-
-	TreeNode root;
-	
-	//Say if b is higher up, and on the same side of the tree, than a, b will be LCA
-	public TreeNode LCA( TreeNode root, TreeNode a, TreeNode b) {
-		if( root == null) {
-			return null;
-		}
-		if( root == a || root ==b) {   
-			return root; 
-		}
+	public class BinaryTree{
+		BinaryTreeNode root;
 		
-		TreeNode left = LCA(root.left, a, b);
-		TreeNode right = LCA(root.right, a, b);
-		
-		//If left && right are not null, this 'root' is LCA for a and b
-		if( left!=null && right!=null) {
-			return root;
-		}
-		if(left == null) {
-			return right;
-		}
-		else {
-			return left;
+		//Say if b is higher up, and on the same side of the tree, than a, b will be LCA
+		public BinaryTreeNode LCA( BinaryTreeNode root, BinaryTreeNode a, BinaryTreeNode b) {
+			if( root == null) {
+				return null;
+			}
+			if( root == a || root ==b) {   
+				return root; 
+			}
+			
+			BinaryTreeNode left = LCA(root.left, a, b);
+			BinaryTreeNode right = LCA(root.right, a, b);
+			
+			
+			//If left && right are not null, this 'root' is LCA for a and b
+			if( left!=null && right!=null) {
+				return root;
+			}
+			if(left == null) {
+				return right;
+			}
+			else {
+				return left;
+			}
 		}
 	}
 	
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		LowestCommonAncestor myTree = new LowestCommonAncestor(); //myTee constructed & shown below in comments
-		myTree.root = new TreeNode(1);                         //1
-		myTree.root.left = new TreeNode(2);                  //2   3
-        myTree.root.right = new TreeNode(3);               //4  5  6 7
-        myTree.root.left.left = new TreeNode(4);    
-        myTree.root.left.right = new TreeNode(5); 
-        myTree.root.right.left = new TreeNode(6); 
-        myTree.root.right.right = new TreeNode(7); 
-        
-        System.out.println("LCA(4, 5): " + myTree.LCA(myTree.root, myTree.root.left.left,myTree.root.left.right).data); 
-        System.out.println("LCA(4, 6): " + myTree.LCA(myTree.root, myTree.root.left.left,myTree.root.right.left).data); 
-        System.out.println("LCA(3, 4): " + myTree.LCA(myTree.root, myTree.root.right, myTree.root.left.left).data); 
-        System.out.println("LCA(2, 4): " + myTree.LCA(myTree.root, myTree.root.left, myTree.root.left.left).data); 
-        System.out.println("LCA(1, 4): " +  myTree.LCA(myTree.root, myTree.root, myTree.root.left.left).data);
+	public class DAGTree{
+		DAGTreeNode root;
 	}
-
 }
+
+
--- LCAncestor/src/LowestCommonAncestorTest.java
+++ LCAncestor/src/LowestCommonAncestorTest.java
@@ -6,28 +6,30 @@ class LowestCommonAncestorTest {
 
 	@Test
 	void testTree() {
-		LowestCommonAncestor myTree = new LowestCommonAncestor(); //myTee constructed & shown below in comments
-		myTree.root = new TreeNode(1);                         //1
-		myTree.root.left = new TreeNode(2);                  //2   3
-        myTree.root.right = new TreeNode(3);               //4  5  6 7
-        myTree.root.left.left = new TreeNode(4);    
-        myTree.root.left.right = new TreeNode(5); 
-        myTree.root.right.left = new TreeNode(6); 
-        myTree.root.right.right = new TreeNode(7); 
+		LowestCommonAncestor myLCAClass = new LowestCommonAncestor();
+		LowestCommonAncestor.BinaryTree myTree = myLCAClass.new BinaryTree();//myTee constructed & shown below in comments
+		myTree.root = new BinaryTreeNode(1);                         //1
+		myTree.root.left = new BinaryTreeNode(2);                  //2   3
+        myTree.root.right = new BinaryTreeNode(3);               //4  5  6 7
+        myTree.root.left.left = new BinaryTreeNode(4);    
+        myTree.root.left.right = new BinaryTreeNode(5); 
+        myTree.root.right.left = new BinaryTreeNode(6); 
+        myTree.root.right.right = new BinaryTreeNode(7); 
         
         assertEquals(5, myTree.root.left.right.data);
 	}
 
 	@Test
 	void testLCA() {
-		LowestCommonAncestor myTree = new LowestCommonAncestor(); //myTee constructed & shown below in comments
-		myTree.root = new TreeNode(1);                         //1
-		myTree.root.left = new TreeNode(2);                  //2   3
-        myTree.root.right = new TreeNode(3);               //4  5  6 7
-        myTree.root.left.left = new TreeNode(4);    
-        myTree.root.left.right = new TreeNode(5); 
-        myTree.root.right.left = new TreeNode(6); 
-        myTree.root.right.right = new TreeNode(7); 
+		LowestCommonAncestor myLCAClass = new LowestCommonAncestor();
+		LowestCommonAncestor.BinaryTree myTree = myLCAClass.new BinaryTree();//myTee constructed & shown below in comments
+		myTree.root = new BinaryTreeNode(1);                         //1
+		myTree.root.left = new BinaryTreeNode(2);                  //2   3
+        myTree.root.right = new BinaryTreeNode(3);               //4  5  6 7
+        myTree.root.left.left = new BinaryTreeNode(4);    
+        myTree.root.left.right = new BinaryTreeNode(5); 
+        myTree.root.right.left = new BinaryTreeNode(6); 
+        myTree.root.right.right = new BinaryTreeNode(7); 
         
         assertEquals(2, myTree.LCA(myTree.root, myTree.root.left.left,myTree.root.left.right).data);
         assertEquals(1 , myTree.LCA(myTree.root, myTree.root.left.left,myTree.root.right.left).data);
@@ -38,7 +40,9 @@ class LowestCommonAncestorTest {
 	
 	@Test
 	void testEmptyTree() {
-		LowestCommonAncestor myTree = new LowestCommonAncestor();
+		LowestCommonAncestor myLCAClass = new LowestCommonAncestor();
+		LowestCommonAncestor.BinaryTree myTree = myLCAClass.new BinaryTree();
+		
 		assertEquals(null, myTree.root);
 		assertEquals(null, myTree.LCA(myTree.root, null, null)); 
 		//Not possible to do myTree.root.right as will cause Null Pointer exception
@@ -46,16 +50,35 @@ class LowestCommonAncestorTest {
 	
 	@Test
 	void testNodeNotInTreeCall() { //Will cause errors if its 'parent' node does not exist too
-		LowestCommonAncestor myTree = new LowestCommonAncestor();
-		myTree.root = new TreeNode(1);  
+		LowestCommonAncestor myLCAClass = new LowestCommonAncestor();
+		LowestCommonAncestor.BinaryTree myTree = myLCAClass.new BinaryTree();
+		
+		myTree.root = new BinaryTreeNode(1);  
 		assertEquals(null, myTree.root.right);
 		assertEquals(null, myTree.root.left);		
 	}
 	
 	@Test
 	void testNotIntNodeData() { //Won't work for double, but seems to work for chars for some reason
-		LowestCommonAncestor myTree = new LowestCommonAncestor();
-		myTree.root = new TreeNode('c');  
+		LowestCommonAncestor myLCAClass = new LowestCommonAncestor();
+		LowestCommonAncestor.BinaryTree myTree = myLCAClass.new BinaryTree();
+		myTree.root = new BinaryTreeNode('c');  
 		assertEquals('c', myTree.root.data);
 	}
+	
+	@Test
+	void testDAG() {
+		LowestCommonAncestor myLCAClass = new LowestCommonAncestor();
+		LowestCommonAncestor.DAGTree myTree = myLCAClass.new DAGTree(); //myTee constructed & shown below in comments
+		
+		myTree.root = new DAGTreeNode(1);                         
+		myTree.root.successors.add(new DAGTreeNode(2));
+		myTree.root.successors.get(0).successors.add(new DAGTreeNode(3));
+        
+        assertEquals(1, myTree.root.data);
+        assertEquals(false, myTree.root.successors.isEmpty());
+        assertEquals(2, myTree.root.successors.get(0).data);
+        assertEquals(3, myTree.root.successors.get(0).successors.get(0).data);
+	}
+
 }
