ff460b4e587fb451acd9bb259aef97758af0de70
haskinsm <60662825+haskinsm@users.noreply.github.com>
2020-11-08 14:06:41 +0000

    Added basic structure for DAG Tree, not finished DAGLCA yet

--- LCAncestor/src/LowestCommonAncestor.java
+++ LCAncestor/src/LowestCommonAncestor.java
@@ -24,7 +24,7 @@ class BinaryTreeNode{
 
 public class LowestCommonAncestor {
 	public class BinaryTree{
-		BinaryTreeNode root;
+		BinaryTreeNode root; //****Might set this to null
 		
 		//Say if b is higher up, and on the same side of the tree, than a, b will be LCA
 		public BinaryTreeNode LCA( BinaryTreeNode root, BinaryTreeNode a, BinaryTreeNode b) {
@@ -52,8 +52,41 @@ public class LowestCommonAncestor {
 		}
 	}
 	
+	//Might be better to include DAGTreeNode class in the below class
 	public class DAGTree{
-		DAGTreeNode root;
+		DAGTreeNode root = null; //////
+		
+		public void addNode(ArrayList<DAGTreeNode> parentsOfNode, int key) {
+			DAGTreeNode node = new DAGTreeNode(key);
+			if(root == null) {
+				root = node;
+			}
+			if(	parentsOfNode != null) { // If null does not necessarily mean its the root of the tree/graph (I think)
+				for(DAGTreeNode parent : parentsOfNode) {
+					if(parent != null) { //Might not need this check, or maybe if it is null could create the parent node
+						parent.successors.add(node);
+					}
+				}
+			}
+		}
+		
+		public ArrayList<DAGTreeNode> getSuccessors(DAGTreeNode node){
+			if(node != null) {
+				return node.successors;
+			}
+			return null;
+		}
+		
+		//Might be better to return the key and take in the keys as parameters rather than nodes.
+		public DAGTreeNode DAGLCA(DAGTree myTree, DAGTreeNode a, DAGTreeNode b) {
+			if( myTree == null || a == null || b == null) {
+				return null;
+			}
+			return null;
+		}
+		
+		
+		
 	}
 }
 
